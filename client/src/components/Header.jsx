5f3466b5d39eae27a7f039b1559b6e9ce02b24d3
Hemanth-1323 <hemanthdhamodharan1323@gmail.com>
2024-07-02 15:00:40 +0530

    create and run a server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -365,7 +365,7 @@ export default function Header() {
           <FaMoon className="text-gray-600" />
         </button>
         <Link to="/sign-in">
-          <button className="px-4 py-2 rounded-lg bg-blue-400 text-gray-100">
+          <button className="px-4 py-2 rounded-lg bg-blue-400 text-gray-800">
             Sign In
           </button>
         </Link>
